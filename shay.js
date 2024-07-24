dab3e011bf90a4f8172f2f95a24a8efa2a274e08
Legacy <highgateshay@yahoo.com>
2024-07-23 20:05:09 -0500

    Created card

--- shay.js
+++ shay.js
@@ -3,72 +3,83 @@ const form = document.querySelector(".bookBtn");
 const books = document.querySelector(".addbook")
 const cancel = document.querySelector(".cancel");
 const viewLibrary = document.querySelector(".library");
-const inputValue = document.querySelector(".inputData");
+const inputValues = document.querySelector(".inputData");
 const inputText = document.getElementById("title");
-const displayBook = document.querySelector(".show-Books");
+const displayBook = document.querySelector(".btn");
 const show = document.querySelector(".book-form");
-let para = document.createElement("p")
-
-
-
-
-
+const library = document.querySelector("a");
+const cardTitle = document.querySelector(".card-title")
+const bookname = document.querySelector(".bookName")
+const authorPara = document.querySelector(".authorName")
+const authorname = document.querySelector("#author")
+const pageNum = document.querySelector("#pages")
 
 const myLibrary = [];
 
-
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-}
-
-
-function addToLibrary() {
-    
-}
-
-function showForm() {
-    para.textContent = "this is a test"
-    inputValue.appendChild(para)
-    inputValue.textContent = para.innerText
+function Book() {
 
 }
 
-function cancelForm() {
-    show.style.display = "none"
-    form.style.display = "block"
-}
-
-displayBook.addEventListener("click", (e) => {
-  inputValue.textContent = "test"
-    
-})
-
-
-
-
-
-
-
-
-
-
-books.addEventListener("click", showForm)
-cancel.addEventListener("click", cancelForm)
-
-
-let a, b, rest;
-[a, b] = [5, 10];
-
-
-[a, b, ...rest] = [5, 10, 3, "This is a test", 25];
-
-function displayIt() {
-    let seeIt = rest[1];
-    inputValue.textContent = seeIt;
+function addbookToLibrary() {
 
 }
 
-displayIt()
+function getCard() {
+   let title = document.createElement("h4")
+   let titlePara = document.createElement("p")
+   let author = document.createElement("h4")
+   let authorPara = document.createElement("p")
+   let pageCount = document.createElement("h4")
+   let pagePara = document.createElement("p")
+   inputValues.style.display = "flex"
+   inputValues.style.paddingLeft = "20px"
+   inputValues.style.paddingTop = "7px"
+   inputValues.style.fontWeight = "bold"
+   inputValues.style.color = "white"
+   cardTitle.appendChild(title)
+   title.textContent = "Title:"
+   cardTitle.appendChild(titlePara)
+   titlePara.textContent = inputText.value
+   cardTitle.appendChild(author)
+   author.textContent = "Author:"
+   cardTitle.appendChild(authorPara)
+   authorPara.textContent = authorname.value
+   cardTitle.appendChild(pageCount)
+   pageCount.textContent = "Pages:"
+   cardTitle.appendChild(pagePara)
+   pagePara.textContent = pageNum.value
+
+
+}
+
+function getForm() {
+    show.style.display = "block"
+    inputValues.style.display = "none"
+    books.style.display = "none"
+    library.style.display = "none"
+}
+
+// function getValues() {
+//     bookname.textContent = inputText.value
+//     inputValues.append(bookname.textContent)
+//     inputValues.innerText = bookname.textContent
+// }
+
+displayBook.addEventListener("click", getCard)
+books.addEventListener("click", getForm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
